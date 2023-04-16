e1a044cb981aa7a1f3dea591502cbe35c0e3d8c5
merlinemris <merdanchariyarv@gmail.com>
2023-04-16 14:36:41 +0500

    feat: admin page update for control

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from .models import Site
+
+admin.site.unregister(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()  # type: Set[str]
+
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+                'user_permissions',
+            }
+
+        # Prevent non-superusers from editing their own permissions
+        if (
+            not is_superuser
+            and obj is not None
+            and obj == request.user
+        ):
+            disabled_fields |= {
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+
+        return form
+
 
 # Register your models here.
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url', 'is_active')
+    list_display_links = ('id', 'name')
+    list_filter = ('is_active',)
+    list_editable = ('is_active',)
+    search_fields = ('name', 'url')
+    ordering = ('-id',)
+    save_as = True
+    exclude = ('user',)
+
+
+    def get_queryset(self, request):
+        qs = super(SiteAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
--- monitor/models.py
+++ monitor/models.py
@@ -5,5 +5,9 @@ class Site(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     url = models.URLField(max_length=300)
+    is_active = models.BooleanField(default=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.url

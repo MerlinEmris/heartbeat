3dcdd8f76e882c6cc3432e545dab7da9fd08ca59
merlinemris <merdanchariyarv@gmail.com>
2023-04-16 13:46:56 +0500

    feat: add sites model

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
-
 # Create your models here.
+class Site(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=300)
+    def __str__(self):
+        return self.url
